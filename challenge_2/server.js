f1f7d96a8989ee8918d1c1869bd89b87b0b745bd
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-16 11:41:21 -0700

    Use express server to serve up html for generator

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const morgan = require('morgan');
 
+app = express();
+app.use(express.static('./client'));
 
+app.use(parser.json());
+app.use(morgan('dev'));
+
+port = 3000;
+app.listen(port);
+console.log(`CSV istening on port ${port}...`);
+
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, './client/index.html'));
+});
