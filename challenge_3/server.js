c659fc6832e5a2c6276bc9fc16489d738f9b0a6e
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-17 12:10:41 -0700

    Create boilerplate for babel transpiler for react

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+
+app.use(express.static('./public'));
+
+const port = 3000;
+app.listen(port);
+console.log(`Express server listening on port ${port}...`);
 
 
+app.get('/compiled/src/index.js', (req,res) => {
+  res.sendFile(path.join(__dirname, '/compiled/src/index.js'));
+})
+
+app.get('/compiled/src/components/app.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/compiled/src/components/app.js'));
+})
---
+++ challenge_3/src/components/app.jsx
@@ -0,0 +1,21 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      purchases: []
+    }
+  }
+
+
+
+  render() {
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
