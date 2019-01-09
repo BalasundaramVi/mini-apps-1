7e25c5176ff4e99812ace1459da71762316d7dd4
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-08 16:55:26 -0800

    Creates 8 x 8 checkers board

--- challenge_b1/client/src/components/App.jsx
+++ challenge_b1/client/src/components/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import createBoard from '../../utils';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      board: [],
+      board: createBoard(),
     };
   }
 
   render() {
     const { board } = this.state;
     return (
-      <div className='application'>
+      <div className="application">
         <h1>HELLO WORLD</h1>
       </div>
     );
---
+++ challenge_b1/client/utils/index.js
@@ -0,0 +1,43 @@
+const createBoard = () => {
+  const board = [];
+  for (let i = 0; i < 8; i += 1) {
+    const row = [];
+    for (let j = 0; j < 8; j += 1) {
+      row.push({
+        piece: null,
+        king: false,
+      });
+    }
+    board.push(row);
+  }
+
+  board[0][1].piece = 'white';
+  board[0][3].piece = 'white';
+  board[0][5].piece = 'white';
+  board[0][7].piece = 'white';
+  board[1][0].piece = 'white';
+  board[1][2].piece = 'white';
+  board[1][4].piece = 'white';
+  board[1][6].piece = 'white';
+  board[2][1].piece = 'white';
+  board[2][3].piece = 'white';
+  board[2][5].piece = 'white';
+  board[2][7].piece = 'white';
+
+  board[5][0].piece = 'red';
+  board[5][2].piece = 'red';
+  board[5][4].piece = 'red';
+  board[5][6].piece = 'red';
+  board[6][1].piece = 'red';
+  board[6][3].piece = 'red';
+  board[6][5].piece = 'red';
+  board[6][7].piece = 'red';
+  board[7][0].piece = 'red';
+  board[7][2].piece = 'red';
+  board[7][4].piece = 'red';
+  board[7][6].piece = 'red';
+
+  return board;
+};
+
+export default createBoard;
