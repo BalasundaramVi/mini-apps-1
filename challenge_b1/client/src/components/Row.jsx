c06be5acab3226c8e958e585bc9527a69fba240e
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-10 18:46:29 -0800

    Reformat tiles for different checkers piece imgs

--- challenge_b1/client/src/components/Row.jsx
+++ challenge_b1/client/src/components/Row.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import RED_PIECE from '../../assets/red_piece.png';
 import '../styles/Row.css';
 
 const Row = ({ row }) => (
   <div className="inner-row">
-    {row.map((square, i) => (
-      <div className={`square col_${i}`} key={`col_${i + 1}`}>
-        <div className="inner-square">{square.piece || 'null'}</div>
-      </div>
-    ))}
+    {row.map((square, i) => {
+      let piece;
+      if (square.piece === 'red') {
+        piece = <div className="red piece">RED</div>;
+      } else if (square.piece === 'white') {
+        piece = <div>WHITE</div>
+      } else {
+        piece = <div>NULL</div>;
+      }
+
+      return (
+        <div className={`square col_${i}`} key={`col_${i + 1}`}>
+          <div className="inner-square">{piece}</div>
+        </div>
+      );
+    })}
   </div>
 );
 
